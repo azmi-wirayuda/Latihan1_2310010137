5e828661c459666a6aaf9b835b8b145c668da626
azmi-wirayuda <azmiwirayuda602@gmail.com>
2025-06-10 14:02:31 +0800

    isial commit

--- src/latihan2/DataDiri.java
+++ src/latihan2/DataDiri.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package latihan2;
 
-/**
- *
- * @author Asus
- */
 public class DataDiri {
     
+public class datadiri {
+    // Atribut
+    private String nama;
+    private String npm;
+    private String kelas;
+
+    // Konstruktor
+    public datadiri(String nama, String npm, String kelas) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+
+    // Getter (opsional)
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    // Setter (opsional)
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+}
+
 }
